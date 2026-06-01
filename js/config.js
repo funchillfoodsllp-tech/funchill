2f18fbaef2559bb0aaa327da9a41de89561e6674
funchillfoodsllp-tech <funchillfoodsllp@gmail.com>
2026-06-01 12:05:19 +0530

    add new config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-const EMAILJS_PUBLIC_KEY = "NUvomUIZk4zXC9x9R";
-const EMAILJS_SERVICE_ID = "service_3re8d0b";
-const EMAILJS_ADMIN_TEMPLATE = "template_8rcgnv7";
-const EMAILJS_REPLY_TEMPLATE = "template_c5bhrr6";
+const EMAILJS_PUBLIC_KEY = "36-N1D_lka6vh-Zv8";
+const EMAILJS_SERVICE_ID = "service_b7hin7l";
+const EMAILJS_ADMIN_TEMPLATE = "template_y8wikdf";
+const EMAILJS_REPLY_TEMPLATE = "template_qmudvhh";
 
 /* Initialize EmailJS */
 emailjs.init(EMAILJS_PUBLIC_KEY);
